a5365397565b9ad3ee9173366d0c43407775e5cc
shidephen <shidephen@outlook.com>
2016-08-26 16:00:34 +0800

    完成音频处理线程类

--- Source/Threads.cpp
+++ Source/Threads.cpp
@@ -9,3 +9,43 @@
 */
 
 #include "Threads.h"
+#include <mutex>
+
+using namespace std;
+
+ProcessingThread::ProcessingThread(
+	VstClientSlim& client, 
+	std::mutex& mu, 
+	std::condition_variable& cond, 
+	std::vector<message>& queue)
+	: Thread("ProcessingThread"),
+	_client(client),
+	_mutex(mu),
+	_cond(cond),
+	_messages(queue)
+{
+
+}
+
+ProcessingThread::~ProcessingThread()
+{
+
+}
+
+void ProcessingThread::run()
+{
+	unique_lock<mutex> lock(_mutex);
+	bool willRun = true;
+	while (willRun)
+	{
+		_cond.wait(lock, [&]() {
+			return !_messages.empty();
+		});
+
+		
+		for (auto it = _messages.begin(); it != _messages.end(); ++it)
+			willRun = _client.ProcessMessage(*it);
+
+		lock.unlock();
+	}
+}
--- Source/Threads.h
+++ Source/Threads.h
@@ -11,7 +11,30 @@
 #ifndef THREADS_H_INCLUDED
 #define THREADS_H_INCLUDED
 
+#include "JuceHeader.h"
+#include <condition_variable>
+#include <vector>
+#include "RemoteClient.h"
 
+class ProcessingThread : Thread
+{
+public:
+	ProcessingThread(
+		VstClientSlim&,
+		std::mutex&,
+		std::condition_variable&,
+		std::vector<message>&);
+	~ProcessingThread();
+	virtual void run() override;
+
+private:
+	std::condition_variable& _cond;
+	std::mutex& _mutex;
+	VstClientSlim& _client;
+	std::vector<message>& _messages;
+
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ProcessingThread);
+};
 
 
 
