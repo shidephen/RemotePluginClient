4cd086f417fdf17b9e8142de790810e085cfd774
shidephen <shidephen@outlook.com>
2016-08-30 16:53:13 +0800

    完成LMMS监控线程

--- Source/Threads.cpp
+++ Source/Threads.cpp
@@ -9,7 +9,11 @@
 */
 
 #include "Threads.h"
+#include <TlHelp32.h>
 #include <mutex>
+#include <cassert>
+#include <exception>
+#include <iostream>
 
 using namespace std;
 
@@ -35,8 +39,8 @@ ProcessingThread::~ProcessingThread()
 void ProcessingThread::run()
 {
 	unique_lock<mutex> lock(_mutex);
-	bool willRun = true;
-	while (willRun)
+
+	while (!threadShouldExit())
 	{
 		_cond.wait(lock, [&]() {
 			return !_messages.empty();
@@ -44,8 +48,68 @@ void ProcessingThread::run()
 
 		
 		for (auto it = _messages.begin(); it != _messages.end(); ++it)
-			willRun = _client.ProcessMessage(*it);
+			_client.ProcessMessage(*it);
 
+		_messages.clear();
 		lock.unlock();
 	}
+}
+
+
+DWORD GetParentProcessID(unsigned int currentPid)
+{
+	HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	PROCESSENTRY32W pe;
+	pe.dwSize = sizeof(pe);
+	if (!Process32FirstW(snap, &pe))
+		return 0;
+
+	while (Process32NextW(snap, &pe))
+	{
+		if (pe.th32ProcessID == currentPid)
+			break;
+	}
+	CloseHandle(snap);
+
+	return pe.th32ParentProcessID;
+}
+
+
+
+LMMSMonitorThread::LMMSMonitorThread(volatile bool& exitCondition) 
+	: _exit_signal(exitCondition),
+	Thread("LMMSMonitorThread")
+{
+	DWORD lmms_pid = GetParentProcessID(GetCurrentProcessId());
+	cout << "LMMS pid: " << lmms_pid << endl;
+
+	_parent = OpenProcess(SYNCHRONIZE, true, lmms_pid);
+	if (_parent <= 0)
+	{
+		DWORD dwErrNo = GetLastError();
+		LPVOID lpMsgBuf;
+		FormatMessage(
+			FORMAT_MESSAGE_ALLOCATE_BUFFER |
+			FORMAT_MESSAGE_FROM_SYSTEM |
+			FORMAT_MESSAGE_IGNORE_INSERTS,
+			NULL,
+			dwErrNo,
+			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+			(LPTSTR)&lpMsgBuf,
+			0, NULL);
+
+		throw new exception((const char*)lpMsgBuf);
+	}
+}
+
+void LMMSMonitorThread::run()
+{
+	DWORD state;
+	do 
+	{
+		state = WaitForSingleObject(_parent, 1000);
+	} while (!threadShouldExit() && state == WAIT_TIMEOUT);
+	
+	if (state == WAIT_OBJECT_0)
+		_exit_signal = true;
 }
--- Source/Threads.h
+++ Source/Threads.h
@@ -16,7 +16,8 @@
 #include <vector>
 #include "RemoteClient.h"
 
-class ProcessingThread : Thread
+// 音频处理线程
+class ProcessingThread : public Thread
 {
 public:
 	ProcessingThread(
@@ -36,6 +37,18 @@ private:
 	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(ProcessingThread);
 };
 
+// LMMS监控线程（如果LMMS退出，Host进程也退出）
+class LMMSMonitorThread : public Thread 
+{
 
+public:
+	explicit LMMSMonitorThread(volatile bool& exitCondition);
+
+	virtual void run() override;
+
+private:
+	volatile bool& _exit_signal;
+	HANDLE _parent;
+};
 
 #endif  // THREADS_H_INCLUDED
