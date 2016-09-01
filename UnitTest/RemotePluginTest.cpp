9a1d19b8477e07f461aafbd49b4ffde7f7ae6bfa
shidephen <shidephen@outlook.com>
2016-09-01 20:44:17 +0800

    1. 完成插件加载卸载测试 2. 添加Chunk数据保存和加载测试 3. 添加插件设置加载和保存的测试

--- UnitTest/RemotePluginTest.cpp
+++ UnitTest/RemotePluginTest.cpp
@@ -12,5 +12,47 @@ TEST(ClientTest, TestConstruction)
 
 TEST(ClientTest, TestLoadPlugin)
 {
-	FAIL();
+	FakeRemotePlugin server;
+	VstClientSlim vst_client(server.KeyIn(), server.KeyOut());
+
+	ASSERT_TRUE(vst_client.LoadPlugin(
+		"E:\\Projects\\RemotePluginClient\\UnitTest\\Minihost.dll"));
+
+	ASSERT_TRUE(vst_client.IsInitialized());
+}
+
+TEST(ClientTest, TestSaveLoadPreset)
+{
+	const char* preset_file = "preset.fxb";
+	FakeRemotePlugin server;
+	VstClientSlim vst_client(server.KeyIn(), server.KeyOut());
+
+	vst_client.LoadPlugin(
+		"E:\\Projects\\RemotePluginClient\\UnitTest\\Minihost.dll");
+
+	ASSERT_TRUE(vst_client.SaveChuckToFile(preset_file));
+	
+	File f(preset_file);
+	ASSERT_TRUE(f.existsAsFile());
+
+	EXPECT_TRUE(vst_client.LoadChuckFromFile(preset_file));
+	f.deleteFile();
+}
+
+TEST(ClientTest, TestSaveLoadSettings)
+{
+	const char* preset_file = "preset.fxp";
+	FakeRemotePlugin server;
+	VstClientSlim vst_client(server.KeyIn(), server.KeyOut());
+
+	vst_client.LoadPlugin(
+		"E:\\Projects\\RemotePluginClient\\UnitTest\\Minihost.dll");
+
+	ASSERT_TRUE(vst_client.SaveSettingsToFile(preset_file));
+
+	File f(preset_file);
+	ASSERT_TRUE(f.existsAsFile());
+
+	EXPECT_TRUE(vst_client.LoadSettingsFromFile(preset_file));
+	f.deleteFile();
 }
