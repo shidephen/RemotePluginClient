032f86d912909a44dd96e96c2f3de747e58c0ea4
shidephen <shidephen@outlook.com>
2016-09-01 12:57:50 +0800

    增加RemoteClient测试

---
+++ UnitTest/RemotePluginTest.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "RemoteClient.h"
+
+const char* plugin_path = "E:\\Projects\\RemotePluginClient\\UnitTest\\Minihost.dll";
+
+TEST(ClientTest, TestConstruction)
+{
+
+}
+
+TEST(ClientTest, TestLoadPlugin)
+{
+	FAIL();
+}
